d02ae8824a919cc09258074d502b20f276cf2f43
fatuunreal <faturach03@gmail.com>
2025-06-24 20:58:42 +0700

    Update stream-deteksi-sms.py

--- stream-deteksi-sms.py
+++ stream-deteksi-sms.py
@@ -10,7 +10,10 @@ tfidf = TfidfVectorizer
 loaded_vec = TfidfVectorizer(decode_error="replace", vocabulary=set(pickle.load(open("new_selected_feature_tf-idf.sav", "rb"))))
 
 # judul
+st.title("NATURAL LANGUAGE PROCESSING A11.4617")
 st.title("Deteksi SMS Spam")
+st.write("FATU RAHMAT A11.2022.14831")
+st.write("ANDI LAKSONO A11.2022.14839")
 
 clean_teks = st.text_input('Masukkan Teks SMS')
 
